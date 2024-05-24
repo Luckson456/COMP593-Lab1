934338ec1d1c1423b67b1542ececeefb3ff21e6e
Luckson456 <lpaija@flemingcollege.ca>
2024-05-24 16:57:40 -0400

    modified both files in the repo clone
    -deleted am print() statement from script1.py
    -added two print () statement to script2.py

--- script2.py
+++ script2.py
@@ -1,2 +1,4 @@
 print("Flat is better than complex--that's fact.")
 print("Unless explicitly silenced--true.")
+print("Simple is better than complex--obviously.")
+print("Beautiful is better than ugly--id it.")
